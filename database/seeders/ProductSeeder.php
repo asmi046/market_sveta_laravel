ead6ce342cbb3ac0e105748be22f88b315e67108
asmi046 <asmi046@gmail.com>
2022-07-28 20:43:47 +0300

    Чтение из xml

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -17,7 +17,11 @@ class ProductSeeder extends Seeder
         $xmlFile = file_get_contents('https://marketsveta.su/product_base/xml/119047.xml');
         $xmlObject = simplexml_load_string($xmlFile);
         $jsonFormatData = json_encode($xmlObject);
-        $result = json_decode($jsonFormatData, true);
-        dd($result); 
+        $result = json_decode($jsonFormatData, false);
+
+        // for ($i = 0;  $i < count($result["shop"]["categories"]); $i++)
+        // var_dump($result["shop"]["offers"]["offer"][0]["name"]);
+        dd((string)$xmlObject->shop->categories->category[0]); 
+        // dd($result->shop->categories->category[0]); 
     }
 }
