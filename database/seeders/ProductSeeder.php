fffa6b177950b18319eec16819fbdbd967157576
asmi046 <asmi046@gmail.com>
2022-08-02 18:35:51 +0300

    Все поля в сиде

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -27,10 +27,23 @@ class ProductSeeder extends Seeder
         for ($i = 0; $i < count($xmlObject->shop->offers->offer); $i++){
             $result[] = [
                 "name" => (string)$xmlObject->shop->offers->offer[$i]->name,
+                "price" => 1000,
+                "price_old" => 1000,
                 "manufacture_status" => "В наличии",
                 "insklad" => (int)$xmlObject->shop->offers->offer[$i]->quantity,
                 "sku" => (string)$xmlObject->shop->offers->offer[$i]->vendorCode,
                 "brand" => (string)$xmlObject->shop->offers->offer[$i]->vendor,
+                "state" => "",
+                "collection" => "",
+                "style" => "",
+                "quote" => "",
+                "description" => "",
+                "cat1" => "",
+                "cat2" => "",
+                "cat3" => "",
+                "img" => "",
+                "title_seo" => "",
+                "description_seo" => "",
             ];
         }
 
