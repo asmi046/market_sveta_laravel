882fd3726f2f55f61406f31a49be394725b4f2e0
asmi046 <asmi046@gmail.com>
2022-08-15 10:50:16 +0300

    новые поля

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -25,7 +25,21 @@ class ProductSeeder extends Seeder
             $all_cat[(string)$xmlObject->shop->categories->category[$i]["id"]] = ["name" => (string)$xmlObject->shop->categories->category[$i], "parentId" => (string)$xmlObject->shop->categories->category[$i]["parentId"]];
 
         for ($i = 0; $i < count($xmlObject->shop->offers->offer); $i++){
-            $result[] = [
+            $first_picture = $xmlObject->shop->offers->offer[$i]->picture;
+            $first_picture = (is_array($first_picture))?(string)$first_picture[0]:(string)$first_picture;    
+            
+            $cat1 = $all_cat[(int)$xmlObject->shop->offers->offer[$i]->categoryId];
+
+            if (!empty($cat1["parentId"]))
+                $cat2 = $all_cat[$cat1["parentId"]];
+            
+            if (!empty($cat2["parentId"]))
+                $cat3 = $all_cat[$cat2["parentId"]];
+            
+            if (!empty($cat3["parentId"]))
+                $cat4 = $all_cat[$cat3["parentId"]];
+                
+            $tmp = [
                 "name" => (string)$xmlObject->shop->offers->offer[$i]->name,
                 "price" => 1000,
                 "price_old" => 1000,
@@ -36,14 +50,15 @@ class ProductSeeder extends Seeder
                 "state" => "",
                 "collection" => "",
                 "style" => "",
-                "quote" => "",
-                "description" => "",
-                "cat1" => "",
-                "cat2" => "",
-                "cat3" => "",
-                "img" => "",
-                "title_seo" => "",
-                "description_seo" => "",
+                "quote" => (string)$xmlObject->shop->offers->offer[$i]->name,
+                "description" => "Купить по выгодной цене - ".(string)$xmlObject->shop->offers->offer[$i]->name,
+                "cat1" => $cat1["name"],
+                "cat2" => (!empty($cat2))?$cat2["name"]:"",
+                "cat3" => (!empty($cat3))?$cat3["name"]:"",
+                "cat4" => (!empty($cat4))?$cat4["name"]:"",
+                "img" => basename($first_picture),
+                "title_seo" => (string)$xmlObject->shop->offers->offer[$i]->name,
+                "description_seo" => "Купить по выгодной цене - ".(string)$xmlObject->shop->offers->offer[$i]->name,
             ];
 
             for ($j = 0; $j<count($xmlObject->shop->offers->offer->param); $j++) 
@@ -52,8 +67,12 @@ class ProductSeeder extends Seeder
                 $value = (string)$xmlObject->shop->offers->offer->param[$j];
                 echo $name." -> ".$value."\n\r";
 
-                if ($name === "Страна происхождения") $result["state"] = $value;
+                if ($name === "Страна происхождения") $tmp["state"] = $value;
+                if ($name === "Коллекция") $tmp["collection"] = $value;
+                if ($name === "Стиль") $tmp["style"] = $value;
             }
+
+            $result[] = $tmp;
 
             var_dump($result);
 
