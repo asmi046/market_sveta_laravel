22615c25471bd9fdeb8720dc501f3aa6adadf8c4
asmi046 <asmi046@gmail.com>
2022-08-18 16:16:04 +0300

    Подключен Axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,16 @@ import {createApp} from 'vue/dist/vue.esm-bundler';
 
 import SearchComponent from "./components/SearchComponent.vue"
 
+import axios from 'axios'
+
+import VueAxios from 'vue-axios'
+
 const app = createApp({
     components:{
        SearchComponent,
     }
-});
+})
+
+app.use(VueAxios, axios)
 
 app.mount("#app");
