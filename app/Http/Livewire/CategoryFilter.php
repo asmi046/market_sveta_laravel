aa25166df9c7ea59736cfdb981fb7148cc4591c6
asmi046 <asmi046@gmail.com>
2022-08-26 20:11:10 +0300

    Компонент фильтра

---
+++ app/Http/Livewire/CategoryFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+
+use Illuminate\Http\Request;
+use App\Filters\ProductFilter;
+
+use App\Models\Product;
+
+class CategoryFilter extends Component
+{
+    use WithPagination;
+    
+    public $cat_id;
+    public $cat_name;
+    public $sub_cat;
+
+    public function get_filter() {
+        $requMain = new Request();
+        $requMain->query->add(['style'=>'хай-тек']);
+
+        $pf = new ProductFilter($requMain);
+        $catProducts = Product::where('cat1', $this->cat_id)
+        ->orWhere('cat2', $this->cat_id)
+        ->orWhere('cat3', $this->cat_id)
+        ->orWhere('cat4', $this->cat_id)->paginate(48);
+
+        dd($requMain->query,  $sql, $catProducts);
+    }
+
+    public function render()
+    {
+        return view('livewire.category-filter');
+    }
+}
