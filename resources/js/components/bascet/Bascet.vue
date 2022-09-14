06aa25c254ffab2cfbecc2d824f07587b8b5666c
asmi046 <asmi046@gmail.com>
2022-09-14 12:31:08 +0300

    Корзина

---
+++ resources/js/components/bascet/Bascet.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="bascet">
+        <div class="bascet_tovar">
+            <div class="control">
+                <a class="clear_bascet_btn" href="#"><span>Очистить корзину</span></a>
+            </div>
+
+            <div class="tovar_list">
+                <div class="tovar">
+                    <div class="tovar_all_blk picture_blk">
+                        <img src="" alt="">
+                    </div>
+                    <div class="tovar_all_blk name_blk">
+                        <h2>Люстра на штанге Covali PL-59936</h2>
+                        <p>Covali (Россия) Артикул: PL-59936</p>
+                    </div>
+                    <div class="tovar_all_blk price_blk">
+                        <span class="rub price_formator">7516</span>
+                    </div>
+                    <div class="tovar_all_blk couint_blk">
+                        <input type="number" value="1">
+                    </div>
+                    <div class="tovar_all_blk summ_blk">
+                        <span class="rub price_formator">207516</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="itogo">
+                <div class="itogo_price_count">
+                    <div class="itogo_row">
+                        <span class="text">Товары (<span>5</span>)</span>
+                        <span class="razd"></span>
+                        <span class="p_price rub price_formator">1325</span>
+                    </div>
+
+                    <div class="itogo_row itogo_row_final">
+                        <span class="text">Итого</span>
+                        <span class="razd"></span>
+                        <span class="p_price rub price_formator">1325</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="bascet_form">
+            <h2>Контактные данные</h2>
+            <form action="GET">
+                <input name="fio" type="text" placeholder="Фамилия, Имя*">
+                <input name="email" type="email" placeholder="e-mail">
+                <input name="phone" type="text" placeholder="Телефон*">
+                <textarea name="adress" placeholder="Адрес"></textarea>
+                <textarea name="comment" placeholder="Комментарий"></textarea>
+                <button class="btn" type="submit">Отправить</button>
+                <p class="policy">Заполняя данную форму и отправляя заказ вы соглашаетесь с <a href="#">политикой конфиденциальности</a></p>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    mounted: function() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
