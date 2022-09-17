5280c2ad4746d13a449ebff097a8dbc62f68d171
asmi046 <asmi046@gmail.com>
2022-09-17 12:10:36 +0300

    Пагинация со своим шаблоном

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -48,24 +48,8 @@
                         <a href="#" class="show-link__btn">Показать еще</a>
                     </div> -->
 
-                    <!-- <nav class="pagination d-flex">
-                        <div class="pagination__nav-links d-flex">
-                            <a class="pagination__back" href="#">Назад</a>
-                            <span class="pagination__numbers">1</span>
-                            <a class="pagination__numbers current" href="#">2</a>
-                            <a class="pagination__numbers" href="#">3</a>
-                            <a class="pagination__numbers" href="#">4</a>
-                            <a class="pagination__numbers" href="#">5</a>
-                            <div class="pagination__block-dot d-flex">
-                                <span class="pagination__dot">.</span>
-                                <span class="pagination__dot">.</span>
-                                <span class="pagination__dot">.</span>
-                            </div>
-                            <a class="pagination__numbers" href="#">60</a>
-                            <a class="pagination__next" href="#">Вперед</a>
-                        </div>
-                    </nav> -->
 
+                    {{$cat_product->links('parts.pagination');}}
                 </main>
 
             </div>
---
+++ resources/views/parts/pagination.blade.php
@@ -0,0 +1,17 @@
+                    <nav class="pagination d-flex">
+                        <div class="pagination__nav-links d-flex">
+                            <a class="pagination__back" href="#">Назад</a>
+                            <span class="pagination__numbers">1</span>
+                            <a class="pagination__numbers current" href="#">2</a>
+                            <a class="pagination__numbers" href="#">3</a>
+                            <a class="pagination__numbers" href="#">4</a>
+                            <a class="pagination__numbers" href="#">5</a>
+                            <div class="pagination__block-dot d-flex">
+                                <span class="pagination__dot">.</span>
+                                <span class="pagination__dot">.</span>
+                                <span class="pagination__dot">.</span>
+                            </div>
+                            <a class="pagination__numbers" href="#">60</a>
+                            <a class="pagination__next" href="#">Вперед</a>
+                        </div>
+                    </nav> 
