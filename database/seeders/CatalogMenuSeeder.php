0813e774f4364dfbd0479c313fcfb9a5fb6b725c
asmi046 <asmi046@gmail.com>
2022-11-07 01:35:59 +0300

    --

--- database/seeders/CatalogMenuSeeder.php
+++ database/seeders/CatalogMenuSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class CatalogMenuSeeder extends Seeder
