a00d27e488a0cf928c03d71fbbbec95b4dee84b2
asmi046 <asmi046@gmail.com>
2022-11-07 00:56:19 +0300

    Цены

--- database/seeders/CatalogMenuSeeder.php
+++ database/seeders/CatalogMenuSeeder.php
@@ -22,6 +22,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 1,
                 "slug" => Str::slug("Люстры"),
                 "title" => "Люстры",
+                "img" => "lyustry.png",
                 "link" => "/category/potolocnye-liustry-2868",
             ],
             
@@ -29,6 +30,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 2,
                 "slug" => Str::slug("Светильники"),
                 "title" => "Светильники",
+                "img" => "svetilniki.png",
                 "link" => "/category/potolocnye-svetilniki-2872",
             ],
 
@@ -36,6 +38,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 3,
                 "slug" => Str::slug("Бра"),
                 "title" => "Бра",
+                "img" => "bra-i-podsvetki.png",
                 "link" => "/category/nastennye-bra-2864",
             ],
 
@@ -43,6 +46,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 4,
                 "slug" => Str::slug("Споты"),
                 "title" => "Споты",
+                "img" => "spoty.png",
                 "link" => "/category/spoty-2893",
             ],
 
@@ -50,6 +54,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 5,
                 "slug" => Str::slug("Настольные лампы"),
                 "title" => "Настольные лампы",
+                "img" => "nastolnye-lampy.png",
                 "link" => "/category/nastolnye-lampy-2865",
             ],
 
@@ -57,6 +62,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 6,
                 "slug" => Str::slug("Торшеры"),
                 "title" => "Торшеры",
+                "img" => "torshery.png",
                 "link" => "/category/torsery-i-napolnye-svetilniki-2894",
             ],
 
@@ -64,6 +70,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 7,
                 "slug" => Str::slug("Трековые системы"),
                 "title" => "Трековые системы",
+                "img" => "trekovye-sistemy.png",
                 "link" => "/category/sinnye-i-strunnye-sistemy-2899",
             ],
 
@@ -71,6 +78,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 8,
                 "slug" => Str::slug("Уличьное освещение"),
                 "title" => "Уличьное освещение",
+                "img" => "eksterernoe_osveshchenie.png",
                 "link" => "/category/svet-dlia-ulicy-2915",
             ],
 
@@ -78,6 +86,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 9,
                 "slug" => Str::slug("Офисное освещение"),
                 "title" => "Офисное освещение",
+                "img" => "ofisnoe-osveshchenie.png",
                 "link" => "/category/texniceskii-svet-10207",
             ],
 
@@ -85,6 +94,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 10,
                 "slug" => Str::slug("Лампочьки"),
                 "title" => "Лампочьки",
+                "img" => "lampochki.png",
                 "link" => "/category/lampy-2908",
             ],
 
@@ -92,6 +102,7 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 11,
                 "slug" => Str::slug("Светодиодная подсветка"),
                 "title" => "Светодиодная подсветка",
+                "img" => "svetodiodnaya-podsvetka.png",
                 "link" => "/category/svetodiodnye-lenty-2884",
             ],
 
@@ -99,9 +110,24 @@ class CatalogMenuSeeder extends Seeder
                 "order" => 12,
                 "slug" => Str::slug("Электротовары"),
                 "title" => "Электротовары",
+                "img" => "elektrotovary.png",
                 "link" => "/category/elektroustanovka-3201",
             ],
         ];
+
+        
+        Storage::disk('local')->put("public/catalog/lyustry.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/28e/lyustry.png"), 'public');
+        Storage::disk('local')->put("public/catalog/svetilniki.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/f99/svetilniki.png"), 'public');
+        Storage::disk('local')->put("public/catalog/bra-i-podsvetki.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/26b/bra-i-podsvetki.png"), 'public');
+        Storage::disk('local')->put("public/catalog/spoty.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/377/spoty.png"), 'public');
+        Storage::disk('local')->put("public/catalog/nastolnye-lampy.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/fa1/nastolnye-lampy.png"), 'public');
+        Storage::disk('local')->put("public/catalog/torshery.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/3a2/torshery.png"), 'public');
+        Storage::disk('local')->put("public/catalog/trekovye-sistemy.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/690/trekovye-sistemy.png"), 'public');
+        Storage::disk('local')->put("public/catalog/eksterernoe_osveshchenie.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/9f3/eksterernoe_osveshchenie.png"), 'public');
+        Storage::disk('local')->put("public/catalog/ofisnoe-osveshchenie.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/1c3/ofisnoe-osveshchenie.png"), 'public');
+        Storage::disk('local')->put("public/catalog/lampochki.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/03e/lampochki.png"), 'public');
+        Storage::disk('local')->put("public/catalog/svetodiodnaya-podsvetka.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/44b/svetodiodnaya-podsvetka.png"), 'public');
+        Storage::disk('local')->put("public/catalog/elektrotovary.png", file_get_contents("https://www.vamsvet.ru/upload/iblock/923/elektrotovary.png"), 'public');
 
         DB::table("catalog_menus")->insert($all_punkt);
     }
---
+++ database/seeders/PriceLoader.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use DB;
+
+class PriceLoader extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ini_set('max_execution_time', 9000);
+        
+        $dir = base_path() . '/public/all_price';;
+        $files = @scandir($dir,1);
+
+        for ($i = 0; $i<count($files); $i++)
+        if (($files[$i] !== ".")&&($files[$i] !== "..")&&(!empty($files[$i]))) { 
+            $row = 0;
+
+            if (($handle = fopen($dir."/".$files[$i], "r")) !== FALSE) {
+                echo  $files[$i]."\n\r"; 
+                while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+                    if ($row == 0) {$row++; continue;}
+                    
+                        if (empty($data) || empty($data[0])) continue;
+        
+                        $sku = $data[0];
+                        $count = $data[2];
+                        $price = str_replace(",",".",$data[1]);
+                        
+                        if (empty($sku)) continue;
+                        
+                        $rez = DB::table('products')->where('sku', $sku)->update(['price' => $price, "insklad" => $count, "price_old" => 0]);
+        
+        
+                        echo  $row . ": " . $sku." - ".$rez."\n\r"; 
+                    
+                    $row++; 
+                }
+        
+            fclose($handle);
+            // unlink($dir."/".$files[$i]); 
+            }   
+        }
+
+    }
+}
