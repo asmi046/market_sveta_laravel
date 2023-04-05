83a8befac99d64f9aeed9c76500a64d4c76da5a9
asmi046 <asmi046@gmail.com>
2023-04-05 21:45:53 +0300

    Окно отправки проекта

--- app/Http/Controllers/SenderController.php
+++ app/Http/Controllers/SenderController.php
@@ -6,9 +6,21 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Mail;
 use App\Mail\RecollMail;
+use App\Mail\ProjectMail;
 
 class SenderController extends Controller
 {
+    public function send_project(Request $request) {
+
+        $data = $request->validate([
+            "name" => ['required','string']
+            "phone" => ['required','string']
+        ]);
+
+        Mail::to(["asmi046@gmail.com", "zakaz@lightsnab.ru", "sale@lightsnab.ru"])->send(new ProjectMail($data));
+        return ["Сообщение отправлено"];
+    }
+
     public function send_consultation(Request $request) {
         $data = $request->validate([
             "phone" => ['required','string']
